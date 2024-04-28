39aedf219548fe3cd5f7604337ed0790a676e43a
Jemmyy007 <ilyavorona2004@mail.ru>
2024-04-28 21:08:25 +0300

    Админка

---
+++ adminka/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require("body-parser")
+const mainRoute = require('./routes/main');
+const gamesRouter = require('./routes/games'); 
+
+const PORT = 3000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public'))); 
+// Запускаем
+app.use(mainRoute, gamesRouter); 
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT http://localhost:${PORT}`);
+})
